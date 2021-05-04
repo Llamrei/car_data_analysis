89d3dd5a3a9dc4fb2fc8d8b9ef15e5762f5a929b
Llamrei <alex@larionov.co.uk>
2021-05-04 18:58:23 +0100

    fixed dedup code

--- remove_dups_from_data.R
+++ remove_dups_from_data.R
@@ -1,6 +1,11 @@
-data <- read.csv('/home/alexander/projects/car_data_analysis/final_project_data/data.csv')
+library("dplyr")
+
+data <- read.csv('/home/alexander/projects/car_data_analysis/final_project_data/data.csv', stringsAsFactors = TRUE)
 data <- na.omit(data)
-incomparables = c(id, desc)
 a <- subset(data, select=-c(id, desc))
 a <- unique(a)
 data <- data[rownames(a),]
+data <- data %>% 
+  group_by(make) %>%
+  filter(n() > 25)
+write.csv(data, "/home/alexander/projects/car_data_analysis/final_project_data/deduped_data.csv", row.names = FALSE)
