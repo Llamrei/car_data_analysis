90e9db480a37d608139e72d80c3e92b4cc703246
Llamrei <al3615@ic.ac.uk>
2021-05-05 13:47:51 +0100

    working single run submissions

--- statml_mini_proj_2.sh
+++ statml_mini_proj_2.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+#PBS -o /rds/general/user/al3615/home/car_data_analysis/pbs_junk
+#PBS -e /rds/general/user/al3615/home/car_data_analysis/pbs_junk
 # Debug queue:
 #PBS -l select=1:ncpus=8:mem=32gb:ompthreads=8
 #PBS -l walltime=00:29:59
-# PBS -J 1-16
+#PBS -J 1-6
 
 # Throughput queue:
 # PBS -l select=1:ncpus=8:mem=24gb:ompthreads=8
-# PBS -l walltime=02:00:0
-# PBS -J 1-16
+# PBS -l walltime=23:00:0
+# PBS -J 1-180
 
 # GPU queue:
 # PBS -lselect=1:ncpus=4:mem=24gb:ngpus=1:gpu_type=RTX6000
@@ -18,18 +20,12 @@ echo "Started"
 echo "StatML Mini-project 2 - al3615"
 date
 set -x
-job="${PBS_JOBID%[*}"
+job="${PBS_JOBID%.*}"
+job="${job%[*}"
 working_dir=$HOME/car_data_analysis
 
 # exp_vals will give the name of the scripts to run with the correct models
 exp_vals=($(ls -d $working_dir/scripts/*))
-extension=${exp_val#*.}
-
-if [[ $extension == "r" ]]; then
-    script_handler=Rscript
-elif [[ $extension == "py" ]]; then
-    script_handler=python
-fi
 
 echo "Array idx $PBS_ARRAY_INDEX"
 if [[ ! -z "$PBS_ARRAY_INDEX" ]]; then
@@ -44,9 +40,22 @@ else
     exp_val=${exp_vals[0]}
     echo "Not in array job, exp val $exp_val"
 fi
+# exp_val="$working_dir/scripts/NLP.py"
+# array_idx=0
+
+extension=${exp_val#*.}
+method_name=`basename ${exp_val%.*}`
+progress_file="${method_name}_progress.txt"
+
+if [[ $extension == "r" ]]; then
+    script_handler=Rscript
+elif [[ $extension == "py" ]]; then
+    script_handler=python
+fi
 
 input_data_dir="$working_dir/final_project_data"
 output_dir="$working_dir/results"
+progress_file="$output_dir/$progress_file"
 
 mkdir -p $output_dir
 
@@ -59,7 +68,14 @@ source activate tf2-text-and-r
 export TFHUB_CACHE_DIR=$EPHEMERAL/.cache/tfhub_modules
 mkdir -p $TFHUB_CACHE_DIR
 
+touch $progress_file
 $script_handler $exp_val $job $array_idx $input_data_dir $output_dir
+
+# Update counter in progress file for how many tests we have
+# use flock for atomic transaction
+( flock -x 200;
+$(expr $(cat "$progress_file") + 1) > $progress_file;
+) 200>/rds/general/user/al3615/home/car_data_analysis/lockfile 
 
 echo "Done"
 date
